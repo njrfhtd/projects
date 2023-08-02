317f99857ee5646183231d7a0734608e97ddbdb7
at <at@iteratia.com>
2023-08-03 01:52:59 +0300

    remove commented code

--- src/app/store/projects/projects.state.ts
+++ src/app/store/projects/projects.state.ts
@@ -16,33 +16,7 @@ export class ProjectsStateModel {
 
 const defaults = {
     globalDisable: false,
-    projects: [
-        // {
-        //     id: '1',
-        //     subject: 'Проект 1',
-        //     description: 'Проект интернет-платформы для рынка лизинга',
-        //     createdBy: 'Александр Иванов',
-        //     startDate: '2021-09-24T09:00:00',
-        //     endDate: '2021-10-24T09:00:00',
-        //     cost: 120000.5
-        // }, {
-        //     id: '2',
-        //     subject: 'Проект 2',
-        //     description: 'Бизнес-мессенджер',
-        //     createdBy: 'Марина Ионова',
-        //     startDate: '2021-10-01T09:00:00',
-        //     endDate: '2021-11-14T09:00:00',
-        //     cost: 720400
-        // }, {
-        //     id: '3',
-        //     subject: 'Проект 3',
-        //     description: 'Проект VR-планетария',
-        //     createdBy: 'Андрей Линев',
-        //     startDate: '2022-02-05T09:00:00',
-        //     endDate: '2022-02-25T09:00:00',
-        //     cost: 80300.25
-        // }
-    ],
+    projects: [],
     selectedProject: null,
 };
 
@@ -53,7 +27,6 @@ const defaults = {
 @Injectable()
 export class ProjectsState {
     constructor(
-        // private utilService: UtilService,
     ) {
     }
 
